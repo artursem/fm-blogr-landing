8d5b28ce5f42f2716c5e5569a44fb04ea3ed8f44
artursem <artur.semeniuk@gmail.com>
2021-06-29 19:55:05 +0200

    mobile nav fix

--- menu.js
+++ menu.js
@@ -1,8 +1,17 @@
 const burger = document.querySelector('#burger');
-const nav = document.querySelector('nav');
-
+const burgerIcon = burger.querySelector('img');
+const nav = document.querySelector('.header__nav');
+let isOpen = false;
 const menuOpen = () => {
-    console.log('open');
+    if (isOpen) {
+        burgerIcon.src = './images/icon-hamburger.svg';
+        nav.style.display = 'none';
+        isOpen = false;
+    } else {
+        burgerIcon.src = './images/icon-close.svg';
+        nav.style.display = 'block'
+        isOpen = true;
+    }
 
 }
 
