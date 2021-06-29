6f134bd74d4795b23e1cc8314926b28414b83c1a
artursem <artur.semeniuk@gmail.com>
2021-06-29 19:41:36 +0200

    add mobile nav

---
+++ menu.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('#burger');
+const nav = document.querySelector('nav');
+
+const menuOpen = () => {
+    console.log('open');
+
+}
+
+burger.addEventListener('click', menuOpen);
